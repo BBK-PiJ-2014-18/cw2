899abe406550834064aa1429c188affa2e15b7bf
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2014-11-16 18:31:23 +0000

    after first explore of files

--- Fraction.java
+++ Fraction.java
@@ -11,16 +11,23 @@ public class Fraction {
 	    // this should use exceptions
             return;
         }
+        // myGcd returns the largest number that goes into both num & denom
         int gcd = myGcd(num, denom);
+   //     System.out.println("num: " + num + " denom: " + denom + " gcd: " + gcd);
+        // and then we divide both top and bottom by the gcd.
+        // it also puts any negative on the denom
         setNumerator(num / gcd);
         setDenominator(denom / gcd);
     }
 
+	// simple print of a fraction
     @Override
     public String toString() {
         return "" + getNumerator() + '/' + getDenominator();
+    		// will need to improve for then denominator is 1
     }
 
+	//getters & setters
     public int getNumerator() {
         return numerator;
     }
@@ -37,6 +44,7 @@ public class Fraction {
         denominator = num;
     }
 
+	//tests if firstFraction.equals(secondFraction)
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -50,13 +58,15 @@ public class Fraction {
         return true;
     }
 
-    @Override
+	// NO IDEA WHAT THIS IS...
+	@Override
     public int hashCode() {
         int result = numerator;
         result = 31 * result + denominator;
         return result;
     }
-
+	
+	//simple return of thisFraction.TIMES(theOtherFraction).
     public Fraction multiply(Fraction other) {
 
         int num = this.getNumerator() * other.getNumerator();
@@ -64,6 +74,8 @@ public class Fraction {
         return new Fraction(num, denom);
     }
 
+	// myGcd returns the largest number that goes into both num & denom
+	// and is negative if need to adjust signs.
     private int myGcd(int a, int b) {
         while (b != 0) {
             int t = b;
--- FractionTest.java
+++ FractionTest.java
@@ -7,18 +7,40 @@ public class FractionTest {
         // test divide by zero - should print an error and exit
         new Fraction(1, 0);
         // test multiply
-	Fraction f = new Fraction(3,10);
-	Fraction g = new Fraction(1,2);
-	Fraction h = new Fraction(3,5);
-	if (!f.equals(g.multiply(h))) System.out.println("Multiply failed");
+		Fraction f = new Fraction(3,10);
+		Fraction g = new Fraction(1,2);
+		Fraction h = new Fraction(3,5);
+		if (!f.equals(g.multiply(h))) System.out.println("Multiply failed (NOT OK)");
+        f= new Fraction(4,10);
+        g = new Fraction(1,2);
+        h = new Fraction (3,5);
+        if (!f.equals(g.multiply(h))) System.out.println("Multiply failed (OK!)");
+        f= new Fraction(30,100);
+        g = new Fraction(1,2);
+        h = new Fraction (3,5);
+        if (!f.equals(g.multiply(h))) System.out.println("Multiply failed (NOT OK)");
+        
         // test equals
-	test(new Fraction(1, 2),new Fraction(1, 2),"error test 1");
-	test(new Fraction(1, 2),new Fraction(3, 6),"error test 2");
-	test(new Fraction(-1, 2),new Fraction(1, -2),"error test 3");
-	test(new Fraction(-1, -2),new Fraction(1, 2),"error test 4");
-	test(new Fraction(4, -8),new Fraction(1, 2),"error test 5");
+		test(new Fraction(1, 2),new Fraction(1, 2),"error test 1");
+		test(new Fraction(1, 2),new Fraction(3, 6),"error test 2");
+		test(new Fraction(-1, 2),new Fraction(1, -2),"error test 3");
+		test(new Fraction(-1, -2),new Fraction(1, 2),"error test 4");
+		test(new Fraction(4, -8),new Fraction(1, 2),"error test 5");
 
         // extend with extra tests
+        
+        //exploring Fraction (incl myGcd) & to.String output
+        System.out.println (">>>> Exploring Fraction <<<<");
+		System.out.println ("3/10 =\t" + new Fraction(3, 10).toString());
+	   	System.out.println ("5/10 =\t" + new Fraction(5, 10).toString());
+    	System.out.println ("13/10 =\t" + new Fraction(13, 10).toString());
+	   	System.out.println ("3/1 =\t" + new Fraction(3, 1).toString());
+        System.out.println ("6/24 =\t" + new Fraction(6, 24).toString());
+        System.out.println ("6/12 =\t" + new Fraction(6, 12).toString());
+    	System.out.println ("9/12 =\t" + new Fraction(9, 12).toString());
+    	System.out.println ("-1/2 =\t" + new Fraction(-1, 2).toString());
+    	System.out.println ("1/-2 =\t" + new Fraction(1,-2).toString());
+    	System.out.println ("-1/-2 =\t" + new Fraction(-1,-2).toString());
     }
 
     static void test(Fraction f1, Fraction f2, String msg){
