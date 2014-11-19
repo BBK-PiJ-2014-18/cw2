4d531b360e4f9413c20b1039bcfb3269b1a3abca
Mark Kingsbury <lking18@dcs.bbk.ac.uk>
2014-11-19 15:59:34 +0000

    getting next fraction or next operation

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -15,12 +15,124 @@ public class FractionCalculator {
 	public Fraction evaluate(Fraction fraction, String inputString) {
 		
 		calcValue = fraction;
-	
-
+		System.out.println();
+		System.out.println("Input String starts as: " + inputString);
+		inputString = getStringPart(inputString);
+		System.out.println("Input String ends as: " + inputString);	
+		System.out.println("Remembered Operation is: " + rememberedOperation);
+		if(nextFraction != null) {
+			System.out.println("Next Fraction is: " + nextFraction.toString());
+		} else {
+			System.out.println("Next Fraction is empty");
+		}
+		System.out.println();
 		return calcValue;
-	
+
 	}
 
+	/**
+	*	load the next operation or fraction from the string
+	*	into rememberedOperation or nextFraction then
+	*	remove that item from the front of the string and 
+	*	return the remainder of the string
+	*/
+	
+	private String getStringPart(String inputString) {
+	
+		String result = "";
+		int i = 0;
+		while(inputString.charAt(i) != ' ' && i < inputString.length()) {
+			i++;
+		}	
+		//nextItem is the next operation or fraction in the string
+		String nextItem = inputString.substring(0,i);
+		//result is input string with nextItem removed from front
+		result = inputString.substring(i+1, inputString.length()); 
+		//check what next item is and store it in nextFraction or rememberedOperation
+		Boolean nextItemCheck = checkNextItem(nextItem);
+		
+		//return the inputString minus the nextItem
+		return result;
+	}
 
+	//THIS IS MESSY... WHY NEED TO WORK WITH CHAR RATHER THAN STR?? 
+	private boolean checkNextItem(String nextItem) {
+	
+		if(nextItem.length() == 1) {
+			//if nextItem is an operation store it as that & return true
+			if(nextItem.charAt(0) == '+' || nextItem.charAt(0) == '-' 
+					|| nextItem.charAt(0) == '*' || nextItem.charAt(0) == '/') {
+				rememberedOperation = nextItem;
+				return true;
+			}
+		}	
+		// if nextItem contains "/" store it as nextFraction & return true
+		if(isFraction(nextItem)) {
+			int num = getNumPart(nextItem);
+			int denom = getDenomPart(nextItem);
+			nextFraction = new Fraction(num, denom);
+			return true;			
+		}
+		
+		// add more.....
+		
+		
+		// if none of above check out ok, return false - we have invalid input
+		return false;	
+	}
+
+	private boolean isFraction(String nextItem) {
+		int countDivideSigns = 0;
+		int positionOfDivideSign = 0;
+		//check only and only one divide sign		
+		for (int i = 0 ; i < nextItem.length() ; i++ ) {
+			if(nextItem.charAt(i) == '/') {
+				positionOfDivideSign = i;
+				countDivideSigns ++;
+			}	
+		}
+		if(countDivideSigns != 1) {
+			return false;
+		}
+		//check numerator part is digits
+		boolean result = true;
+		for (int i = 0 ; i < positionOfDivideSign ; i++) {
+			char c = (char) nextItem.charAt(i);
+			if(!Character.isDigit(c)) {
+				result = false;
+			}
+		}
+		//check denominator part is digits
+		for (int i = positionOfDivideSign + 1; i < nextItem.length() ; i++) {
+		
+			char c = (char) nextItem.charAt(i);
+			if(!Character.isDigit(c)) {
+				result = false;
+			}
+		}			
+		return result;
+	}
+
+	private int getNumPart(String nextItem) {		
+		int result;
+		int i = 0;
+		while(nextItem.charAt(i) != '/') {
+			i++;
+		}
+		String numPart = nextItem.substring(0,i);
+		result = Integer.parseInt(numPart);
+		return result;
+	}
+
+	private int getDenomPart(String nextItem) {
+		int result;
+		int i = 0;
+		while(nextItem.charAt(i) != '/') {
+			i++;
+		}
+		String numPart = nextItem.substring(i+1,nextItem.length());
+		result = Integer.parseInt(numPart);
+		return result;
+	}
 
 }
--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -8,14 +8,32 @@ public class FractionCalculatorTest {
 	private void launch() {
 
 		FractionCalculator test1 = new FractionCalculator();
+		
 
 		//Initial test of set-up structure
+		
 		System.out.println(" <<< Testing passing a fraction and getting it back >>> "); 
-		Fraction startFraction = new Fraction(3,4);
+		Fraction startFraction = new Fraction(1,10);
 		String testString = "a string";
+//		Fraction result = test1.evaluate(startFraction, testString);
+//		System.out.println("Result is: " + result.toString());
+   
+		//Test ability to load first operation or faction from front of inputString
+		//and then remove from front to create new inputString 
+	
+		System.out.println(" <<< Testing find String Part >>> ");
+/*		testString = "+ 3/4 - 7/14 * 2/4";
+		result = test1.evaluate(startFraction, testString);
+		testString = "3/4 - 7/14 * 2/4";
+		result = test1.evaluate(startFraction, testString);
+*/
+		System.out.println(" <<< Testing find String Part and store >>> ");
+		testString = "+ 13/456 - 7/14 * 2/4";
 		Fraction result = test1.evaluate(startFraction, testString);
-		System.out.println("Result is: " + result.toString());
- 
+		testString = "524/4265 - 7/14 * 2/4";
+		result = test1.evaluate(startFraction, testString);
+
+	
 	}
 
 }
